dc3915094d2821b46e4c1a34ee2f3da33381664a
dan-kagi <65138417+dan-kagi@users.noreply.github.com>
2024-07-22 14:15:42 -0400

    add exercise 10

--- index.js
+++ index.js
@@ -1,169 +1,246 @@
 const questions = [
-    {
-        id: "1",
-        question: "Em uma reação o ferro reage com oxigênio para produzir óxido de ferro (II). Calcule a massa deste produto formada na reação de 112 g de ferro com 32 g de oxigênio.",
-        image: "",
-        options: [
-            {isCorrect:true, answer:"144 g de óxido de ferro (II)"},
-            {isCorrect: false, answer:"134 g de óxido de ferro (II)"},
-            {isCorrect:false, answer:"112 g de óxido de ferro (II)"},
-            {isCorrect:false, answer:"11,2 g de óxido de ferro (II)"},
-        ]
-    },
-    {
-        id: "2",
-        question: "No processo de eletrólise da água formam-se gás hidrogênio e gás oxigênio. Recordando a Lei de Conservação de Massas de Lavoisier. Determine a massa de oxigênio formada.",
-        image: "./public/images/fig2.png",
-        options: [
-            {isCorrect:true, answer:"64 g de oxigênio"},
-            {isCorrect: false, answer:"18 g de oxigênio"},
-            {isCorrect:false, answer:"32 g de oxigênio"},
-            {isCorrect:false, answer:"16 g de oxigênio"},
-        ]
-    },
-    {
-        id: "3",
-        question: "Na combustão completa do metano são gerados gás carbônico e água. A tabela abaixo mostra resultados de dois experimentos. Recordando as Leis de Lavoisier e de Proust (Proporções fixas), calcule os valores faltantes.",
-        image: "./public/images/fig3.png",
-        options: [
-            {isCorrect:true, answer:"x = 44 g, y = 16 g, w = 11 g, z = 9 g"},
-            {isCorrect: false, answer:"x = 11 g, y = 4 g, w = 2,75 g, z = 2,25 g"},
-            {isCorrect:false, answer:"x = 88 g, y = 64 g, w = 44 g, z = 16 g"},
-            {isCorrect:false, answer:"x = 22 g, y = 8 g, w = 5,5 g, z = 4,5 g"},
-        ]
-    },
-    {
-        id: "4",
-        question: "Cálcio reage com oxigênio para formar óxido de cálcio. Aplicando as leis de Lavoisier e de Proust, preencha a tabela abaixo para os resultados de dois experimentos. ",
-        image: "./public/images/fig4.png",
-        options: [
-            {isCorrect:true, answer:"t = 8 g, w = 8 g, r = 11,2 g"},
-            {isCorrect: false, answer:"t = 4 g, w = 4 g, r = 5,6 g"},
-            {isCorrect:false, answer:"t = 16 g, w = 16 g, r = 22,4 g"},
-            {isCorrect:false, answer:"t = 8 g, w = 16 g, r = 50 g"},
-        ]
-    },
-    {
-        id: "5",
-        question: "Carbonato de cálcio quando aquecido se decompõe formando gás carbônico e óxido de cálcio. A decomposição completa de 10 g de cabonato de cálcio produziu 4,4 g de gás carbônico. Determine a massa de óxido de cálcio formada nesta reação.",
-        image: "",
-        options: [
-            {isCorrect:true, answer:"5,6 g de óxido de cálcio"},
-            {isCorrect: false, answer:"8,8 g de óxido de cálcio"},
-            {isCorrect:false, answer:"10,0 g de óxido de cálcio"},
-            {isCorrect:false, answer:"3,2 g de óxido de cálcio"},
-        ]
-    },
-    {
-        id: "6",
-        question: "Uma das alternativas para diminuir a quantidade de dióxido de carbono liberada para a atmosfera consiste em borbulhar esse gás em solução aquosa de hidróxido de sódio. A reação que ocorre está representada na equação química abaixo. Em uma reação 44 g de dióxido de carbono reagem com hidróxido de sódio formando 106 g de carbonato de sódio e 18 g de água. Nesta reação determine a massa de hidróxido sódio consumida.",
-        image: "./public/images/fig6.png",
-        options: [
-            {isCorrect:true, answer:"80 g de hidróxido de sódio"},
-            {isCorrect: false, answer:"40 g de hidróxido de sódio"},
-            {isCorrect:false, answer:"8,0 g de hidróxido de sódio"},
-            {isCorrect:false, answer:"20,0 g de hidróxido de sódio"},
-        ]
-    },
-    {
-        id: "7",
-        question: "O gás nitrogênio reage com gás hidrogênio para formar amônia. Observe os dados experimentais abaixo e determine os valores de a, b, c, d e e.",
-        image: "./public/images/fig7.png",
-        options: [
-            {isCorrect:true, answer:"a = 68 g, b = 30 g, c = 140 g, d = 504 g, e = 612 g"},
-            {isCorrect: false, answer:"a = 50 g, b = 250 g, c = 130 g, d = 500 g, e = 300 g"},
-            {isCorrect:false, answer:"a = 68 g, b = 30 g, c = 140 g, d = 500 g, e = 600 g"},
-            {isCorrect:false, answer:"a = 88 g, b = 40 g, c = 140 g, d = 504 g, e = 612 g"},
-        ]
-    },
-    {
-        id: "8",
-        question: "O ácido clorídrico reage com o hidróxido de magnésio com formação de água e cloreto de magnésio. I)	Sabe que na reação de 73 g de ácido clorídrico com 58 g de hidróxido de magnésio são formados 36 g de água. Nesta reação, determine a massa de cloreto de magnésio formada (Lei de Lavoisier). II) Determine qual seria a massa de água, em g, formada na reação de 21,9 g de hidróxido de magnésio (Lei de Proust)",
-        image: "",
-        options: [
-            {isCorrect:true, answer:"I) 95 g de cloreto de magnésio.  II) 13,6 g"},
-            {isCorrect: false, answer:"I) 90 g de cloreto de magnésio.  II) 10,5 g"},
-            {isCorrect:false, answer:"I) 95 g de cloreto de magnésio.  II) 30,6 g"},
-            {isCorrect:false, answer:"I) 98 g de cloreto de magnésio.  II) 14,5 g"},
-        ]
-    },
-    {
-        id: "9",
-        question: "O gás hidrogênio reage com o gás oxigênio para formar água. Determine os dados faltantes.",
-        image: "./public/images/fig9.png",
-        options: [
-            {isCorrect:true, answer:"Exp1. 18 g --- Exp2. 128 g --- Exp3. 25 g --- Exp4. 32 g e 36 g"},
-            {isCorrect: false, answer:"Exp1. 16 g --- Exp2. 125 g --- Exp3. 25 g --- Exp4. 32 g e 36 g"},
-            {isCorrect:false, answer:"Exp1. 18 g --- Exp2. 128 g --- Exp3. 50 g --- Exp4. 34 g e 38 g"},
-            {isCorrect:false, answer:"Exp1. 36 g --- Exp2. 224 g --- Exp3. 25 g --- Exp4. 32 g e 36 g"},
-        ]
-    },
-]
+  {
+    id: '1',
+    question:
+      'Em uma reação o ferro reage com oxigênio para produzir óxido de ferro (II). Calcule a massa deste produto formada na reação de 112 g de ferro com 32 g de oxigênio.',
+    image: '',
+    options: [
+      { isCorrect: true, answer: '144 g de óxido de ferro (II)' },
+      { isCorrect: false, answer: '134 g de óxido de ferro (II)' },
+      { isCorrect: false, answer: '112 g de óxido de ferro (II)' },
+      { isCorrect: false, answer: '11,2 g de óxido de ferro (II)' },
+    ],
+  },
+  {
+    id: '2',
+    question:
+      'No processo de eletrólise da água formam-se gás hidrogênio e gás oxigênio. Recordando a Lei de Conservação de Massas de Lavoisier. Determine a massa de oxigênio formada.',
+    image: './public/images/fig2.png',
+    options: [
+      { isCorrect: true, answer: '64 g de oxigênio' },
+      { isCorrect: false, answer: '18 g de oxigênio' },
+      { isCorrect: false, answer: '32 g de oxigênio' },
+      { isCorrect: false, answer: '16 g de oxigênio' },
+    ],
+  },
+  {
+    id: '3',
+    question:
+      'Na combustão completa do metano são gerados gás carbônico e água. A tabela abaixo mostra resultados de dois experimentos. Recordando as Leis de Lavoisier e de Proust (Proporções fixas), calcule os valores faltantes.',
+    image: './public/images/fig3.png',
+    options: [
+      { isCorrect: true, answer: 'x = 44 g, y = 16 g, w = 11 g, z = 9 g' },
+      { isCorrect: false, answer: 'x = 11 g, y = 4 g, w = 2,75 g, z = 2,25 g' },
+      { isCorrect: false, answer: 'x = 88 g, y = 64 g, w = 44 g, z = 16 g' },
+      { isCorrect: false, answer: 'x = 22 g, y = 8 g, w = 5,5 g, z = 4,5 g' },
+    ],
+  },
+  {
+    id: '4',
+    question:
+      'Cálcio reage com oxigênio para formar óxido de cálcio. Aplicando as leis de Lavoisier e de Proust, preencha a tabela abaixo para os resultados de dois experimentos. ',
+    image: './public/images/fig4.png',
+    options: [
+      { isCorrect: true, answer: 't = 8 g, w = 8 g, r = 11,2 g' },
+      { isCorrect: false, answer: 't = 4 g, w = 4 g, r = 5,6 g' },
+      { isCorrect: false, answer: 't = 16 g, w = 16 g, r = 22,4 g' },
+      { isCorrect: false, answer: 't = 8 g, w = 16 g, r = 50 g' },
+    ],
+  },
+  {
+    id: '5',
+    question:
+      'Carbonato de cálcio quando aquecido se decompõe formando gás carbônico e óxido de cálcio. A decomposição completa de 10 g de cabonato de cálcio produziu 4,4 g de gás carbônico. Determine a massa de óxido de cálcio formada nesta reação.',
+    image: '',
+    options: [
+      { isCorrect: true, answer: '5,6 g de óxido de cálcio' },
+      { isCorrect: false, answer: '8,8 g de óxido de cálcio' },
+      { isCorrect: false, answer: '10,0 g de óxido de cálcio' },
+      { isCorrect: false, answer: '3,2 g de óxido de cálcio' },
+    ],
+  },
+  {
+    id: '6',
+    question:
+      'Uma das alternativas para diminuir a quantidade de dióxido de carbono liberada para a atmosfera consiste em borbulhar esse gás em solução aquosa de hidróxido de sódio. A reação que ocorre está representada na equação química abaixo. Em uma reação 44 g de dióxido de carbono reagem com hidróxido de sódio formando 106 g de carbonato de sódio e 18 g de água. Nesta reação determine a massa de hidróxido sódio consumida.',
+    image: './public/images/fig6.png',
+    options: [
+      { isCorrect: true, answer: '80 g de hidróxido de sódio' },
+      { isCorrect: false, answer: '40 g de hidróxido de sódio' },
+      { isCorrect: false, answer: '8,0 g de hidróxido de sódio' },
+      { isCorrect: false, answer: '20,0 g de hidróxido de sódio' },
+    ],
+  },
+  {
+    id: '7',
+    question:
+      'O gás nitrogênio reage com gás hidrogênio para formar amônia. Observe os dados experimentais abaixo e determine os valores de a, b, c, d e e.',
+    image: './public/images/fig7.png',
+    options: [
+      {
+        isCorrect: true,
+        answer: 'a = 68 g, b = 30 g, c = 140 g, d = 504 g, e = 612 g',
+      },
+      {
+        isCorrect: false,
+        answer: 'a = 50 g, b = 250 g, c = 130 g, d = 500 g, e = 300 g',
+      },
+      {
+        isCorrect: false,
+        answer: 'a = 68 g, b = 30 g, c = 140 g, d = 500 g, e = 600 g',
+      },
+      {
+        isCorrect: false,
+        answer: 'a = 88 g, b = 40 g, c = 140 g, d = 504 g, e = 612 g',
+      },
+    ],
+  },
+  {
+    id: '8',
+    question:
+      'O ácido clorídrico reage com o hidróxido de magnésio com formação de água e cloreto de magnésio. I)	Sabe que na reação de 73 g de ácido clorídrico com 58 g de hidróxido de magnésio são formados 36 g de água. Nesta reação, determine a massa de cloreto de magnésio formada (Lei de Lavoisier). II) Determine qual seria a massa de água, em g, formada na reação de 21,9 g de hidróxido de magnésio (Lei de Proust)',
+    image: '',
+    options: [
+      {
+        isCorrect: true,
+        answer: 'I) 95 g de cloreto de magnésio.  II) 13,6 g',
+      },
+      {
+        isCorrect: false,
+        answer: 'I) 90 g de cloreto de magnésio.  II) 10,5 g',
+      },
+      {
+        isCorrect: false,
+        answer: 'I) 95 g de cloreto de magnésio.  II) 30,6 g',
+      },
+      {
+        isCorrect: false,
+        answer: 'I) 98 g de cloreto de magnésio.  II) 14,5 g',
+      },
+    ],
+  },
+  {
+    id: '9',
+    question:
+      'O gás hidrogênio reage com o gás oxigênio para formar água. Determine os dados faltantes.',
+    image: './public/images/fig9.png',
+    options: [
+      {
+        isCorrect: true,
+        answer:
+          'Exp1. 18 g --- Exp2. 128 g --- Exp3. 25 g --- Exp4. 32 g e 36 g',
+      },
+      {
+        isCorrect: false,
+        answer:
+          'Exp1. 16 g --- Exp2. 125 g --- Exp3. 25 g --- Exp4. 32 g e 36 g',
+      },
+      {
+        isCorrect: false,
+        answer:
+          'Exp1. 18 g --- Exp2. 128 g --- Exp3. 50 g --- Exp4. 34 g e 38 g',
+      },
+      {
+        isCorrect: false,
+        answer:
+          'Exp1. 36 g --- Exp2. 224 g --- Exp3. 25 g --- Exp4. 32 g e 36 g',
+      },
+    ],
+  },
+  {
+    id: '10',
+    question:
+      'Numa reação, 24 g de magnésio reagem com uma certa massa de oxigênio formando 40 g de óxido de magnésio. a) Nesta reação que massa de oxigênio, em gramas, participa da reação? b) Em em experimento, que massa de magnésio reagiria com 20 g de oxigênio?',
+    image: '',
+    options: [
+      {
+        isCorrect: true,
+        answer: 'a) 16 g de oxigênio; b) 30 g de magnésio',
+      },
+      {
+        isCorrect: false,
+        answer: 'a) 16 g de oxigênio; b) 20 g de magnésio',
+      },
+      {
+        isCorrect: false,
+        answer: 'a) 32 g de oxigênio; b) 20 g de magnésio',
+      },
+      {
+        isCorrect: false,
+        answer: 'a) 40 g de oxigênio; b) 30 g de magnésio',
+      },
+    ],
+  },
+];
 
-function shuffle(arr){
-    const length = arr.length;
-    const newArr = []
-    const storedIdx = []
-    for (let i = 0; i < length; i++){
-        let randomIdx = Math.floor(Math.random() * length);
-        while(storedIdx.includes(randomIdx)){
-           randomIdx = Math.floor(Math.random() * length);
-        }
-        newArr.push(arr[randomIdx]); 
-        storedIdx.push(randomIdx);
+function shuffle(arr) {
+  const length = arr.length;
+  const newArr = [];
+  const storedIdx = [];
+  for (let i = 0; i < length; i++) {
+    let randomIdx = Math.floor(Math.random() * length);
+    while (storedIdx.includes(randomIdx)) {
+      randomIdx = Math.floor(Math.random() * length);
     }
-    return newArr;
+    newArr.push(arr[randomIdx]);
+    storedIdx.push(randomIdx);
+  }
+  return newArr;
 }
 
-function checkResult(inputs){
-    let selectedInput = false;
-    const questionId = inputs[0].id
-    inputs.forEach(input => {
-        if (input.checked === true){
-            selectedInput = input;
-        }
-    })
-    if (selectedInput){
-        if (selectedInput.value === "true"){
-            alert(`Q${questionId} - Parabéns, Você Acertou!`)
-        } else {
-            alert(`Q${questionId} - Errado. Resolva o exercício novamente.`)
-        }
+function checkResult(inputs) {
+  let selectedInput = false;
+  const questionId = inputs[0].id;
+  inputs.forEach((input) => {
+    if (input.checked === true) {
+      selectedInput = input;
+    }
+  });
+  if (selectedInput) {
+    if (selectedInput.value === 'true') {
+      alert(`Q${questionId} - Parabéns, Você Acertou!`);
     } else {
-        alert(`Você deve selecionar uma resposta para Q${questionId}!`);
+      alert(`Q${questionId} - Errado. Resolva o exercício novamente.`);
     }
+  } else {
+    alert(`Você deve selecionar uma resposta para Q${questionId}!`);
+  }
 }
 
-function createQuestion(obj){
-    let template = document.getElementsByTagName("template")[0];
-    let card = template.content.cloneNode("true");
-    card.querySelector("p").innerHTML = `<span><em>${obj.id} - </em></span>${obj.question}`
-    card.querySelector("img").setAttribute("src", obj.image);
-    const inputs = card.querySelectorAll("input");
-    const labels = card.querySelectorAll("label");
-    const shuffledOptions = shuffle(obj.options)
-    const button = card.querySelector("button")
-    button.addEventListener("click", (event) => {
-        checkResult(inputs);
-        event.preventDefault();
-    });
-    for (let i = 0; i < 4; i++){
-        inputs[i].value = shuffledOptions[i].isCorrect
-        inputs[i].id = obj.id;
-        labels[i].textContent =  shuffledOptions[i].answer
-    }
-    
-    let container = document.createElement("div");
-    container.setAttribute("class", "card");
-    container.appendChild(card);
-    const main = document.querySelector("#main");
-    main.append(container);
+function createQuestion(obj) {
+  let template = document.getElementsByTagName('template')[0];
+  let card = template.content.cloneNode('true');
+  card.querySelector(
+    'p'
+  ).innerHTML = `<span><em>${obj.id} - </em></span>${obj.question}`;
+  card.querySelector('img').setAttribute('src', obj.image);
+  const inputs = card.querySelectorAll('input');
+  const labels = card.querySelectorAll('label');
+  const shuffledOptions = shuffle(obj.options);
+  const button = card.querySelector('button');
+  button.addEventListener('click', (event) => {
+    checkResult(inputs);
+    event.preventDefault();
+  });
+  for (let i = 0; i < 4; i++) {
+    inputs[i].value = shuffledOptions[i].isCorrect;
+    inputs[i].id = obj.id;
+    labels[i].textContent = shuffledOptions[i].answer;
+  }
+
+  let container = document.createElement('div');
+  container.setAttribute('class', 'card');
+  container.appendChild(card);
+  const main = document.querySelector('#main');
+  main.append(container);
 }
 
-function listAllQuestions(){
-    questions.forEach(createQuestion);
+function listAllQuestions() {
+  questions.forEach(createQuestion);
 }
 
-document.querySelector("footer").innerHTML = `<p>Copyright &copy; Danilo Morais Itokagi. All rights reserved. ${new Date().getFullYear()}</p>`
+document.querySelector(
+  'footer'
+).innerHTML = `<p>Copyright &copy; Danilo Morais Itokagi. All rights reserved. ${new Date().getFullYear()}</p>`;
 
 listAllQuestions();
